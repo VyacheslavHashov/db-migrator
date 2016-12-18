a54014066a143c31e7d779c07cf92bb1c9acb7ac
VyacheslavHashov <vyacheslavhashov@gmail.com>
2016-12-18 03:06:04 +0300

    Traversing graph

--- src/Database/Migrator.hs
+++ src/Database/Migrator.hs
@@ -1,20 +1,54 @@
+{-# language OverloadedStrings #-}
+{-# language FlexibleContexts #-}
+
 module Database.Migrator where
 
+import Data.Monoid
 import qualified Data.Text as T
 import qualified Data.ByteString as B
 import qualified Data.Map as M
+import qualified Data.Set as S
+import Control.Monad
+import Control.Monad.State
+import Data.Tree
+import Data.Char (isAlphaNum)
 
-type MgFolder = T.Text
 type MgNumber = Int
 
-data MigrationName = MigrationName (Maybe MgNumber) T.Text
-
 data Migration = Migration
-    { mgName :: MigrationName
-    , mgContent :: B.ByteString
-    } deriving (Show)
+    { mgFolder  :: T.Text
+    , mgName    :: T.Text
+    } deriving (Eq, Ord)
 
-type MgMap = M.Map MgFolder Migration
+instance Show Migration where
+    show m = T.unpack $ mgFolder m <> "." <> mgName m
+
+type Deps = [Migration]
+type Node = Tree
+
+type MgGraph = [Node Migration]
+
+traverseGraph
+    :: MgGraph
+    -> (S.Set Migration -> Migration -> Bool)
+    -> State (S.Set Migration) [Migration]
+traverseGraph graph f = concat <$> traverse go graph
+  where
+    go (Node x ts) = do
+       s <- get
+       if (x `S.notMember` s) && f s x
+           then do
+             modify (S.insert x)
+             (<> [x]) . concat <$> mapM go ts
+           else pure []
+
+getPath :: MgGraph -> [Migration]
+getPath g =  evalState (traverseGraph g (\_ _ -> True)) S.empty
+
+
+makeGraph :: M.Map Migration Deps -> MgGraph
+makeGraph = undefined
+
 
 -- | All errors
 data Error
@@ -25,23 +59,33 @@ data Error
     -- | Migration exists on disk but was not applied in database
     | Unapplied
     -- | Folder has more than one base migration
-    | MoreOnceBase
+    | MultipleBase
+    -- | Unknown dependency
+    | UnknownDep
     -- | Two different migration in folder have the same migration as previous
     -- In that case merge is need
     | NeedMerge
     -- | Migration with that name already exists
     | DuplicateName
+    -- | Migration file has no SQl commands
+    | EmptyMigration
+
+-- | All warnings
+data Warning
+    -- | Subfolders are ignored
+    = IgnoredSubfolder
 
 data ParseError
     = NoHeader
     | InvalidFormat
 
--- | Migration name should contain only alpha characters and digits
+-- | Migration name should contain only alpha characters, dash and digits
+-- and its length must be non-zero
 isValidMgName :: T.Text -> Bool
-isValidMgName = undefined
+isValidMgName t = T.all (\c -> isAlphaNum c || c == '_') t && not (T.null t)
 
-parseMgName :: T.Text -> MigrationName
-parseMgName = undefined
+-- parseMgName :: T.Text -> MigrationName
+-- parseMgName = undefined
 
 
 -- Config
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
+import Data.Tree
+import Database.Migrator
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+a1 = Node (Migration "a1" "") []
+a2 = Node (Migration "a2" "") [a1]
+a3 = Node (Migration "a3" "") [a2]
+a4 = Node (Migration "a4" "") [a3, b3]
+a5 = Node (Migration "a5" "") [a4]
+b1 = Node (Migration "b1" "") []
+b2 = Node (Migration "b2" "") [b1, c1]
+b3 = Node (Migration "b3" "") [b2]
+b4 = Node (Migration "b4" "") [b3, c3]
+c1 = Node (Migration "c1" "") []
+c2 = Node (Migration "c2" "") [c1]
+c3 = Node (Migration "c3" "") [c2]
+c4 = Node (Migration "c4" "") [c3]
+c5 = Node (Migration "c5" "") [c4]
+
