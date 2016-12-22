6badfc0dca73ba5c1779d0d0a19d43fa4d1fdbb1
VyacheslavHashov <vyacheslavhashov@gmail.com>
2016-12-22 21:03:41 +0300

    Parsing filenames with tests

--- src/Database/Migrator.hs
+++ src/Database/Migrator.hs
@@ -12,11 +12,10 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.Map as M
 import qualified Data.Set as S
-import Data.Char (toLower)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad
 import Control.Monad.State
-import Data.Char (isAlphaNum, isSpace)
+import Data.Char (isAlphaNum, isDigit, isSpace, toLower)
 import Data.Word
 import Data.List (break, stripPrefix)
 import System.FilePath
@@ -27,12 +26,17 @@ import Data.Maybe (fromMaybe)
 -- import qualified System.FilePath.Find as F
 import qualified System.PosixCompat.Files as F
 
+-- | The sequental number of a migration.
 newtype MgNumber = MgNumber Word64
     deriving (Show, Eq, Ord, Read)
 
+-- | The folder name where a migration is located. Must contain only alphabet
+-- chars, digits and '_', '-'
 newtype MgFolder = MgFolder T.Text
     deriving (Show, Eq, Ord)
 
+-- | The desription for a migration. Must contain only alphabet chars, digits
+-- and '_', '-'
 newtype MgDesc   = MgDesc T.Text
     deriving (Show, Eq, Ord)
 
@@ -59,8 +63,23 @@ newtype MgIndex = MgIndex (M.Map Migration MgList)
 data RawMgNode = RawMgNode Migration [MigrationId]
     deriving Show
 
--- instance Show Migration where
---     show m = T.unpack $ mgFolder m <> "." <> mgName m
+-- Build a migration folder validating that it contains only
+-- allowed symbols
+makeMgFolder :: T.Text -> Maybe MgFolder
+makeMgFolder t | valid t   = Just $ MgFolder t
+               | otherwise = Nothing
+  where
+    valid = T.all (\c -> isAlphaNum c || c == '_' || c == '-')
+
+-- Build a migration's description validating that it contains only
+-- allowed symbols
+makeMgDesc :: T.Text -> Maybe MgDesc
+makeMgDesc t | valid t   = Just $ MgDesc t
+             | otherwise = Nothing
+  where
+    valid = T.all (\c -> isAlphaNum c || c == '_' || c == '-')
+
+
 
 migrationId :: Migration -> MigrationId
 migrationId = undefined
@@ -80,8 +99,8 @@ buildPlan (MgNode x (CrossDeps deps):xs) = do
         else pure []
 
 
-getPath :: MgGraph -> [Migration]
-getPath (MgGraph g) =  evalState (fmap concat . traverse buildPlan $ M.elems g) S.empty
+getPlan :: MgGraph -> [Migration]
+getPlan (MgGraph g) =  evalState (fmap concat . traverse buildPlan $ M.elems g) S.empty
 
 data BuildState = BuildState
     { bsRest :: M.Map MgFolder [RawMgNode]
@@ -117,6 +136,9 @@ printMigrationList = M.foldMapWithKey f
   where
     f (MgFolder folder) xs = folder <> ":\n"
         <> T.unlines (map migrationFullname xs )<> "\n"
+
+printPlan :: [Migration] -> T.Text
+printPlan = T.unlines . map migrationFullname
 
 -- | All errors
 data Error
@@ -187,9 +209,9 @@ createMigrationTable = undefined
 applyMigration :: IO ()
 applyMigration = undefined
 
----------
+-----------------------------------------------------------
 -- File system
-
+-----------------------------------------------------------
 
 type FileM = ExceptT MigrationFileError IO
 
@@ -223,16 +245,20 @@ isMigrationFile path = (isSQL &&) <$> isFile
     isFile = F.isRegularFile <$> F.getFileStatus path
 
 -- | Parses a filename in the parts of a migration name.
+--
 -- Filename must start with a number followed by an optional description
 -- separated by the '_' symbol.
 parseFilename
     :: MonadError MigrationFileError m => String -> m (MgNumber, MgDesc)
-parseFilename name = do
+parseFilename name =
     let (rnumber, rdesc) = break (== '_') name
-        desc = MgDesc . T.pack . fromMaybe "" $ stripPrefix "_" rdesc
-    case readMaybe rnumber of
-        Nothing -> throwError $ InvalidFilename $ T.pack name
-        Just n  -> pure (MgNumber n, desc)
+    in (,) <$> readNumber rnumber <*> readDesc rdesc
+  where
+    throwFilenameError = throwError $ InvalidFilename $ T.pack name
+    readDesc ""        = pure $ MgDesc ""
+    readDesc ['_']     = throwFilenameError
+    readDesc ('_':xs)  = maybe throwFilenameError pure $ makeMgDesc $ T.pack xs
+    readNumber = maybe throwFilenameError (pure . MgNumber) . readMaybe
 
 -- | Parses a single dependency
 -- format : <folder>.<number>_<desc>
@@ -261,21 +287,37 @@ parseHeader s = do
         Nothing -> pure []
         Just rest -> traverse parseDependency $ BC.split ',' rest
 
+-----------------------------
 -- TEST
-a1 = MgNode (Migration (MgFolder "a") (MgNumber 1) (MgDesc "a1") ) $ CrossDeps []
-a2 = MgNode (Migration (MgFolder "a") (MgNumber 2) (MgDesc "a2") ) $ CrossDeps []
-a3 = MgNode (Migration (MgFolder "a") (MgNumber 3) (MgDesc "a3") ) $ CrossDeps []
-a4 = MgNode (Migration (MgFolder "a") (MgNumber 4) (MgDesc "a4") ) $ CrossDeps [drop 2 mb]
-a5 = MgNode (Migration (MgFolder "a") (MgNumber 5) (MgDesc "a5") ) $ CrossDeps []
-b1 = MgNode (Migration (MgFolder "b") (MgNumber 1) (MgDesc "b1") ) $ CrossDeps []
-b2 = MgNode (Migration (MgFolder "b") (MgNumber 2) (MgDesc "b2") ) $ CrossDeps [mc]
-b3 = MgNode (Migration (MgFolder "b") (MgNumber 3) (MgDesc "b3") ) $ CrossDeps []
-b4 = MgNode (Migration (MgFolder "b") (MgNumber 4) (MgDesc "b4") ) $ CrossDeps [drop 2 mc]
-c1 = MgNode (Migration (MgFolder "c") (MgNumber 1) (MgDesc "c1") ) $ CrossDeps []
-c2 = MgNode (Migration (MgFolder "c") (MgNumber 2) (MgDesc "c2") ) $ CrossDeps []
-c3 = MgNode (Migration (MgFolder "c") (MgNumber 3) (MgDesc "c3") ) $ CrossDeps []
-c4 = MgNode (Migration (MgFolder "c") (MgNumber 4) (MgDesc "c4") ) $ CrossDeps []
-c5 = MgNode (Migration (MgFolder "c") (MgNumber 5) (MgDesc "c5") ) $ CrossDeps []
+-----------------------------
+a1 = MgNode (Migration (MgFolder "a") (MgNumber 1) (MgDesc "a1") ) $
+        CrossDeps []
+a2 = MgNode (Migration (MgFolder "a") (MgNumber 2) (MgDesc "a2") ) $
+        CrossDeps []
+a3 = MgNode (Migration (MgFolder "a") (MgNumber 3) (MgDesc "a3") ) $
+        CrossDeps []
+a4 = MgNode (Migration (MgFolder "a") (MgNumber 4) (MgDesc "a4") ) $
+        CrossDeps [drop 2 mb]
+a5 = MgNode (Migration (MgFolder "a") (MgNumber 5) (MgDesc "a5") ) $
+        CrossDeps []
+b1 = MgNode (Migration (MgFolder "b") (MgNumber 1) (MgDesc "b1") ) $
+        CrossDeps []
+b2 = MgNode (Migration (MgFolder "b") (MgNumber 2) (MgDesc "b2") ) $
+        CrossDeps [mc]
+b3 = MgNode (Migration (MgFolder "b") (MgNumber 3) (MgDesc "b3") ) $
+        CrossDeps []
+b4 = MgNode (Migration (MgFolder "b") (MgNumber 4) (MgDesc "b4") ) $
+        CrossDeps [drop 2 mc]
+c1 = MgNode (Migration (MgFolder "c") (MgNumber 1) (MgDesc "c1") ) $
+        CrossDeps []
+c2 = MgNode (Migration (MgFolder "c") (MgNumber 2) (MgDesc "c2") ) $
+        CrossDeps []
+c3 = MgNode (Migration (MgFolder "c") (MgNumber 3) (MgDesc "c3") ) $
+        CrossDeps []
+c4 = MgNode (Migration (MgFolder "c") (MgNumber 4) (MgDesc "c4") ) $
+        CrossDeps []
+c5 = MgNode (Migration (MgFolder "c") (MgNumber 5) (MgDesc "c5") ) $
+        CrossDeps []
 
 ma = reverse [a1, a2, a3, a4, a5]
 mb = reverse [b1, b2, b3, b4]
@@ -284,10 +326,17 @@ mc = reverse [c1, c2, c3, c4, c5]
 graph :: MgGraph
 graph = MgGraph $ M.fromList [(MgFolder "A", ma), (MgFolder "B", mb), (MgFolder "C", mc)]
 
-readAndPrint :: IO ()
-readAndPrint = do
+readAndPrintList :: IO ()
+readAndPrintList = do
     r <- runExceptT readFromDisk
     case r of
         Left _ -> error "bad"
         Right g -> T.putStr . printMigrationList . flip listMigrations [] . fst $ buildGraph g
+
+readAndPrintPlan :: IO ()
+readAndPrintPlan = do
+    r <- runExceptT readFromDisk
+    case r of
+        Left _ -> error "bad"
+        Right g -> T.putStr . printPlan . getPlan . fst $ buildGraph g
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,31 +1,33 @@
 import Database.Migrator
 
+import Test.Hspec
+import Data.Either
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    testParsingHeaders
 
-a1 = MgNode (Migration "a1" "") $ CrossDeps []
-a2 = MgNode (Migration "a2" "") $ CrossDeps []
-a3 = MgNode (Migration "a3" "") $ CrossDeps []
-a4 = MgNode (Migration "a4" "") $ CrossDeps [drop 2 mb]
-a5 = MgNode (Migration "a5" "") $ CrossDeps []
-b1 = MgNode (Migration "b1" "") $ CrossDeps []
-b2 = MgNode (Migration "b2" "") $ CrossDeps [mc]
-b3 = MgNode (Migration "b3" "") $ CrossDeps []
-b4 = MgNode (Migration "b4" "") $ CrossDeps [drop 2 mc]
-c1 = MgNode (Migration "c1" "") $ CrossDeps []
-c2 = MgNode (Migration "c2" "") $ CrossDeps []
-c3 = MgNode (Migration "c3" "") $ CrossDeps []
-c4 = MgNode (Migration "c4" "") $ CrossDeps []
-c5 = MgNode (Migration "c5" "") $ CrossDeps []
 
-ma = reverse [a1, a2, a3, a4, a5]
-mb = reverse [b1, b2, b3, b4]
-mc = reverse [c1, c2, c3, c4, c5]
-
-graph :: MgGraph
-graph = M.fromList [("A", ma), ("B", mb), ("C", mc)]
-
--- TODO test for filenames
+testParsingHeaders :: Spec
+testParsingHeaders = describe "parseFilename" $ do
+    it "can parse name as only digits" $
+        assertRight $ parseFilename "0000"
+    it "can parse name with description" $
+        assertRight $ parseFilename "0000_zero"
+    it "should fail on invalid names" $ do
+        assertLeft $ parseFilename "0000zero"
+        assertLeft $ parseFilename "zero"
+        assertLeft $ parseFilename "000_zero second"
+        assertLeft $ parseFilename "0000_"
+        assertLeft $ parseFilename "0000_zero.second"
 -- TODO test for file header
 -- TODO test for reading migrations from disk
+
+
+-- Helpers
+assertRight :: (Show e, Show a) => Either e a -> Expectation
+assertRight = (`shouldSatisfy` isRight)
+
+assertLeft :: (Show e, Show a) => Either e a -> Expectation
+assertLeft  = (`shouldSatisfy` isLeft)
 
